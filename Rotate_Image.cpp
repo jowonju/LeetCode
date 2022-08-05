618f168ede756ecedab9303d311ccd75b8c3f293
WonJu Jo <wonju.cho@digipen.edu>
2022-08-05 12:14:49 +0900

    add a variable for getting the matrix size

--- Rotate_Image.cpp
+++ Rotate_Image.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        for(int i = 0; i < matrix.size(); i++)
+        int size = matrix.size();
+        for(int i = 0; i < size; i++)
         {
-            for(int j = i + 1, index = i + 1; j < matrix.size(); j++, index++)
+            for(int j = i + 1, index = i + 1; j < size; j++, index++)
             {
                 swap(matrix[i][j], matrix[index][i]);
             }
             
         }
         
-        for(int i = 0; i < matrix.size(); i++)
+        for(int i = 0; i < size; i++)
         {
             reverse(matrix[i].begin(), matrix[i].end());
         }
