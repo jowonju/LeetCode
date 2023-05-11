789838d4c868733312490fbafab6616c7e36ffdb
WonJu Jo <wonju.cho@digipen.edu>
2023-05-11 07:21:23 -0700

    implement with the recursion

--- Reverse_string.cpp
+++ Reverse_string.cpp
@@ -5,5 +5,39 @@ public:
         {
             swap(s[i], s[j]);
         }
+
+        //recursion
+         void recursion(vector<char>& s, int pos)
+        {
+            if(pos == s.size() / 2)
+            {
+                return;
+            }
+            
+            swap(s[pos], s[s.size() - pos - 1]);
+            recursion(s, pos + 1);
+            
+        }
+    
+        void reverseString(vector<char>& s) {
+            recursion(s, 0);
+        }
+
+        //recursion - using start and end for the function parameter
+        void recursion(vector<char>& s, int start, int end)
+        {
+            if(start >= end)
+            {
+                return;
+            }
+            
+            swap(s[start], s[end]);
+            recursion(s, start + 1, end - 1);
+            
+        }
+    
+        void reverseString(vector<char>& s) {
+            recursion(s, 0, s.size() - 1);
+        }
     }
 };
